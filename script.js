e70de9736558699077f34774c0ec9e8461d8efc9
Sisilisko <beloborodova.viktoriia@gmail.com>
2023-06-07 22:26:51 +0300

    init: script arrays

---
+++ script.js
@@ -0,0 +1,8 @@
+var who_array = ["minä", "sinä", "hän", "me", "te", "he"];
+var verbi_array = ["nukkua","syödä","herätä","opiskella","lukea","mennä","lähteä","ehtiä"];
+var kello_array=[1,2,3,4,5,6,7,8,9,10,11,12];
+var ehto_array=["tasan","ennen","jälkeen","menessä","asti"]
+
+function get_who() {
+
+}
